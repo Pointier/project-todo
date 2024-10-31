2f3cf4273b923725bfcc1ce4a27c0f09d8377dff
Hugo <hugo.pointier@gmail.com>
2024-10-31 02:54:18 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import AddTask from './addTask'
+
+function App() {
+  return (
+    <>
+      <AddTask />
+    </>
+  )
+}
+
+export default App
