d10f856f621130888e8b3a836cd9d93ed1bc01e8
Hugo <hugo.pointier@gmail.com>
2025-06-06 17:30:27 +0200

    changes to get firebase working on render

--- server/firebaseConfig.ts
+++ server/firebaseConfig.ts
@@ -1,6 +1,10 @@
 import admin from "firebase-admin";
-
-const serviceAccount = require("./toDoAdminKey.json");
+let serviceAccount: any;
+if (process.env.NODE_ENV === "production") {
+  serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY!);
+} else {
+  serviceAccount = require("./toDoAdminKey.json");
+}
 
 admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
 
