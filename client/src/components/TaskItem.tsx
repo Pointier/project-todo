0b347c64a27bb893fee5c628074b88b246db8ef5
Hugo <hugo.pointier@gmail.com>
2025-06-06 18:01:38 +0200

    additional fix to client for prod

--- client/src/components/TaskItem.tsx
+++ client/src/components/TaskItem.tsx
@@ -1,29 +1,22 @@
-import styles from './TaskItem.module.css'
-import { Task } from './AddTask'
-import { format } from 'date-fns';
+import styles from "./TaskItem.module.css";
+import { Task } from "./types/types";
+import { format } from "date-fns";
 
 interface TaskProps {
   task: Task;
   onComplete: (taskId: number) => void;
 }
+// TODO: delete this file ?
 const TaskItem = ({ task, onComplete }: TaskProps) => {
   return (
     <div className={styles.TaskItem}>
-      <div>
-        {task.name}
-      </div>
-      <div>
-        {task.description}
-      </div>
-      <div>
-        {format(task.date, 'dd MMM y')}
-      </div>
+      <div>{task.description}</div>
+      <div>{format(task.date, "dd MMM y")}</div>
       <div>
         <button onClick={() => onComplete(task.id)}>Completed</button>
       </div>
     </div>
-  )
+  );
+};
 
-}
-
-export default TaskItem
+export default TaskItem;
