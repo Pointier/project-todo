16ecc2da56b32df3155f4fb5077423095d71ead4
Hugo <hugo.pointier@gmail.com>
2025-06-06 21:18:11 +0200

    updated connection to db

--- server/dbConnection.ts
+++ server/dbConnection.ts
@@ -5,12 +5,23 @@ import { Pool } from "pg";
 import { usersTable } from "./drizzle/schema/schema";
 config({ path: ".env" });
 
-const pool = new Pool({
-  host: process.env.DB_HOST || "",
-  user: process.env.DB_USER || "",
-  password: process.env.DB_PASSWORD || "",
-  port: Number(process.env.DB_PORT) || 5432,
-  database: process.env.DB_NAME || "",
-});
+const isProduction = process.env.NODE_ENV === "production";
+let pool: Pool;
+if (isProduction && process.env.DB_URL) {
+  pool = new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  pool = new Pool({
+    host: process.env.DB_HOST || "",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PASSWORD || "",
+    port: Number(process.env.DB_PORT) || 5432,
+    database: process.env.DB_NAME || "",
+  });
+}
 
 export const db = drizzle({ client: pool });
